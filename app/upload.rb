c3e94eaa65e776475b779fd5e2dd69e12d679f71
Marko Kacanski <marko.kacanski@gmail.com>
2014-07-27 16:33:32 +0200

    Added jquery uploader to the frontend, and simple 'ok' response to the backend

--- app/upload.rb
+++ app/upload.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'json'
+
+set :root, File.dirname(__FILE__)
 
 get '/' do
-	"Hello World"
+	erb :index
+end
+
+post '/upload' do
+	puts "------------debug params:----------------"
+	puts params.inspect
+	puts "------------content-range:---------------"
+	puts request.env['HTTP_CONTENT_RANGE']
+	puts "-----------------------------------------2"
+	content_type :json
+	{status: 201}.to_json
 end
