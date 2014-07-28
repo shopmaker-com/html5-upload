302dfe3710467d4bef3ae883d3d5bf4fd1012a0d
Marko Kacanski <marko.kacanski@gmail.com>
2014-07-28 11:44:58 +0200

    Added rudimentary chunked uploads

--- app/upload.rb
+++ app/upload.rb
@@ -12,7 +12,16 @@ post '/upload' do
 	puts params.inspect
 	puts "------------content-range:---------------"
 	puts request.env['HTTP_CONTENT_RANGE']
-	puts "-----------------------------------------2"
+	puts "-----------------------------------------"
+
+	temp_file_location = params["files"][0][:tempfile].path
+
+	result = %x(cat #{temp_file_location} >> uploads/upload_test)
+
+	puts "------------filesystem operation result:--"
+	puts result
+	puts "------------------------------------------"
+
 	content_type :json
 	{status: 201}.to_json
 end
