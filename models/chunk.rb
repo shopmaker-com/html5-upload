a48f1bae4727593e5c88abb6467e8d07da2ede10
Johannes Leers <info@gauda.de>
2014-08-08 10:25:23 +0200

    further refactor

--- models/chunk.rb
+++ models/chunk.rb
@@ -1,31 +1,27 @@
 require 'shellwords'
 
 class Chunk
-  def initialize(upload_dir, file_name, temp_file=nil)
-    @file_name = file_name #file[:filename]
-    @upload_dir = upload_dir #Sinatra::Application.settings.upload_dir
+  def initialize(upload_dir, file_name)
+    @file_name = Shellwords.escape(file_name)
+    @upload_dir = Shellwords.escape(upload_dir)
     @file_path = "#{@upload_dir}/#{@file_name}"
-    @partial_file_path = "#{@file_path}.part"
+    @partial_file_path = "#{@upload_dir}/#{@file_name}.part"
   end
 
 	def upload(temp_file, content_range)
-		# extract data about incoming file chunk form the http Content_range header
+		# extract data about incoming file chunk from the http_content_range header
     content_range = content_range.match(/[0-9]+-([0-9]+)\/([0-9]+)/)
     uploaded_bytes = content_range[1].to_i
     expected_bytes = content_range[2].to_i
+    raise "http_content_rage is not set" if uploaded_bytes.zero? || expected_bytes.zero?
 
     #file on disk doesn't have the incoming chunk, append the chunk
-    if file_size < uploaded_bytes
-      #implicit return true if success, false on fail
-      Kernel.system("cat #{temp_file} >> #{Shellwords.escape(@partial_file_path)}")
-    end
+    Kernel.system("cat #{temp_file} >> #{@partial_file_path}") if file_size < uploaded_bytes
 
-		#handle last chunk rename, i.e. remove ".part" from the end of filename
-		if expected_bytes - uploaded_bytes == 1
-			# it's the last chunk, because bytes_uploaded is counted from zero
-			# so when subtracted from the file length difference is one
-			FileUtils.mv Shellwords.escape(@partial_file_path), Shellwords.escape(@file_path)
-		end 
+		#handle last chunk rename (remove ".part" from the end of filename)
+    # it's the last chunk, because uploaded_bytes is counted from zero
+    # so when subtracted from the file length the difference is one
+  	FileUtils.mv @partial_file_path, @file_path if expected_bytes - uploaded_bytes == 1
 
 		{name: @file_name, size: expected_bytes, uploadedBytes: file_size}
   end
